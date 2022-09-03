a0873a28ad32efaed70ed482d6ddb364071cb404
raul <develop.raul@gmail.com>
2022-09-03 21:24:30 +0900

    [FIX] Page 자료형을 List로 전환 및 이에 필요한 수정사항 반영

--- src/main/java/com/gitjupalza/domain/rank/controller/RankController.java
+++ src/main/java/com/gitjupalza/domain/rank/controller/RankController.java
@@ -1,13 +1,12 @@
 package com.gitjupalza.domain.rank.controller;
 
-import com.gitjupalza.domain.member.service.LoginUserService;
-import com.gitjupalza.domain.rank.data.dto.RankingDto;
+import com.gitjupalza.domain.rank.data.dto.DetailedRankingDto;
 import com.gitjupalza.domain.rank.data.response.PageableQueryRankingResponse;
 import com.gitjupalza.domain.rank.data.response.QueryRankingResponse;
-import com.gitjupalza.domain.rank.service.RankService;
+import com.gitjupalza.domain.rank.service.RankingService;
 import com.gitjupalza.domain.rank.util.RankConverter;
+import com.gitjupalza.global.member.service.LoginUserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,19 +14,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping("/api/v1/rank")
 @RequiredArgsConstructor
 public class RankController {
-    private final RankService rankService;
+    private final RankingService rankingService;
     private final RankConverter rankConverter;
     private final LoginUserService loginUserService;
 
     @GetMapping("/list")
     public ResponseEntity<PageableQueryRankingResponse> queryRankings(@RequestParam final Integer size, @RequestParam final Integer page) {
         final PageRequest pageRequest = PageRequest.of(page, size);
-        final Page<RankingDto> dtoPage = rankService.queryRankingsWithPagination(pageRequest);
-        final Page<QueryRankingResponse> responsePage = dtoPage.map(rankConverter::toResponse);
+        final List<DetailedRankingDto> dtoPage = rankingService.queryRankingsWithPagination(pageRequest);
+        final List<QueryRankingResponse> responsePage = dtoPage.stream()
+                .map(rankConverter::toResponse)
+                .collect(Collectors.toList());
         final Long issuerIdx = loginUserService.getLoginUserIdx();
         final PageableQueryRankingResponse response = rankConverter.toResponse(responsePage, issuerIdx);
         return ResponseEntity.ok(response);
--- src/main/java/com/gitjupalza/domain/rank/util/RankConverter.java
+++ src/main/java/com/gitjupalza/domain/rank/util/RankConverter.java
@@ -1,11 +1,14 @@
 package com.gitjupalza.domain.rank.util;
 
-import com.gitjupalza.domain.rank.data.dto.RankingDto;
+import com.gitjupalza.domain.rank.data.dto.DetailedRankingDto;
 import com.gitjupalza.domain.rank.data.response.PageableQueryRankingResponse;
 import com.gitjupalza.domain.rank.data.response.QueryRankingResponse;
-import org.springframework.data.domain.Page;
+import com.gitjupalza.global.chemy.data.dto.ChemyDto;
+
+import java.util.List;
 
 public interface RankConverter {
-    QueryRankingResponse toResponse(RankingDto dto);
-    PageableQueryRankingResponse toResponse(Page<QueryRankingResponse> responsePage, Long issuerIdx);
+    QueryRankingResponse toResponse(DetailedRankingDto dto);
+    PageableQueryRankingResponse toResponse(List<QueryRankingResponse> responsePage, Long issuerIdx);
+    DetailedRankingDto toDetailedDto(Long viewerIdx, ChemyDto dto);
 }
