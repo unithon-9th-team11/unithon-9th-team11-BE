0e534a2e4a4e44fc3b2495655048c9183b8d8a0d
raul <develop.raul@gmail.com>
2022-09-04 06:25:19 +0900

    [ADD] CORS 설정 반영

--- src/main/java/com/gitjupalza/GitjupalzaApplication.java
+++ src/main/java/com/gitjupalza/GitjupalzaApplication.java
@@ -1,9 +1,15 @@
 package com.gitjupalza;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@CrossOrigin("*")
 @SpringBootApplication
 @EnableRedisRepositories
 public class GitjupalzaApplication {
@@ -12,4 +18,13 @@ public class GitjupalzaApplication {
 		SpringApplication.run(GitjupalzaApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(@NotNull CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://front-server.com/", "http://localhost:3000");
+			}
+		};
+	}
 }
