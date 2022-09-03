b1e4ebc328d8da18b265eede8add237a7616ddcd
raul <develop.raul@gmail.com>
2022-09-03 19:21:42 +0900

    [FIX] ChemyServiceImpl구현

--- src/main/java/com/gitjupalza/domain/chemy/util/ChemyConverterImpl.java
+++ src/main/java/com/gitjupalza/domain/chemy/util/ChemyConverterImpl.java
@@ -2,11 +2,14 @@ package com.gitjupalza.domain.chemy.util;
 
 import com.gitjupalza.domain.chemy.data.dto.ChemyDto;
 import com.gitjupalza.domain.chemy.data.entity.Chemy;
+import com.gitjupalza.domain.chemy.data.event.CreateChemyEvent;
+import com.gitjupalza.domain.chemy.data.event.DeleteChemyEvent;
 import com.gitjupalza.domain.chemy.data.response.QueryChemyResponse;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ChemyConverterImpl implements ChemyConverter {
+
     @Override
     public QueryChemyResponse toQueryResponse(ChemyDto dto) {
         return new QueryChemyResponse();
@@ -20,5 +23,15 @@ public class ChemyConverterImpl implements ChemyConverter {
     @Override
     public ChemyDto toDto(Chemy entity) {
         return new ChemyDto(entity.getIdx(), entity.getFirstGithubId(), entity.getSecondGithubId(), entity.getChemyScore());
+    }
+
+    @Override
+    public CreateChemyEvent toCreateEvent(Chemy entity, Long issuerIdx) {
+        return new CreateChemyEvent(issuerIdx, entity.getIdx(), entity.getFirstGithubId(), entity.getSecondGithubId(), entity.getChemyScore());
+    }
+
+    @Override
+    public DeleteChemyEvent toDeleteEvent(Long idx) {
+        return new DeleteChemyEvent(idx);
     }
 }
