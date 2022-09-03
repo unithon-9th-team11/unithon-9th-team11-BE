01194f25e63aca5b7a7c43358d7044a083c85681
raul <develop.raul@gmail.com>
2022-09-03 16:17:05 +0900

    [ADD] Chemy 관련 Database 로직 구현

---
+++ src/main/java/com/gitjupalza/domain/chemy/repository/ChemyRepository.java
@@ -0,0 +1,7 @@
+package com.gitjupalza.domain.chemy.repository;
+
+import com.gitjupalza.domain.chemy.data.entity.Chemy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChemyRepository extends JpaRepository<Chemy, Long> {
+}
