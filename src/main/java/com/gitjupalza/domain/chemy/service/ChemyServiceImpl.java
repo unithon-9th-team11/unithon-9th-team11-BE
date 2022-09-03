8369e1ba1975fc509c148e611abb65d0e5d82d4e
raul <develop.raul@gmail.com>
2022-09-03 19:15:30 +0900

    [FIX] ChemyServiceImpl에 이벤트 퍼블리싱 로직 추가

--- src/main/java/com/gitjupalza/domain/chemy/service/ChemyServiceImpl.java
+++ src/main/java/com/gitjupalza/domain/chemy/service/ChemyServiceImpl.java
@@ -2,11 +2,15 @@ package com.gitjupalza.domain.chemy.service;
 
 import com.gitjupalza.domain.chemy.data.dto.ChemyDto;
 import com.gitjupalza.domain.chemy.data.entity.Chemy;
+import com.gitjupalza.domain.chemy.data.event.CreateChemyEvent;
+import com.gitjupalza.domain.chemy.data.event.DeleteChemyEvent;
 import com.gitjupalza.domain.chemy.exception.ChemyNotFoundException;
 import com.gitjupalza.domain.chemy.repository.ChemyRepository;
 import com.gitjupalza.domain.chemy.util.ChemyConverter;
 import com.gitjupalza.domain.chemy.util.ChemyGenerator;
+import com.gitjupalza.domain.member.service.LoginUserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,12 +19,19 @@ public class ChemyServiceImpl implements ChemyService {
     private final ChemyRepository chemyRepository;
     private final ChemyGenerator chemyGenerator;
     private final ChemyConverter chemyConverter;
+    private final LoginUserService loginUserService;
+    private final ApplicationEventPublisher applicationEventPublisher;
 
     @Override
     public Long createChemy(String firstGithubId, String secondGithubId) {
         final ChemyDto dto = chemyGenerator.generate(firstGithubId, secondGithubId);
         final Chemy entity = chemyConverter.toEntity(dto);
         final Chemy savedEntity = chemyRepository.save(entity);
+
+        final Long issuerIdx = loginUserService.getLoginUserIdx();
+        final CreateChemyEvent event = chemyConverter.toCreateEvent(savedEntity, issuerIdx);
+        applicationEventPublisher.publishEvent(event);
+
         return savedEntity.getIdx();
     }
 
@@ -35,6 +46,10 @@ public class ChemyServiceImpl implements ChemyService {
     public Long deleteChemy(Long idx) {
         if(!chemyRepository.existsById(idx)) throw new ChemyNotFoundException(String.format("idx가 %d인 궁합정보를 찾을 수 없습니다! 궁합정보를 삭제할 수 없습니다!", idx));
         chemyRepository.deleteById(idx);
+
+        DeleteChemyEvent event = chemyConverter.toDeleteEvent(idx);
+        applicationEventPublisher.publishEvent(event);
+
         return idx;
     }
 }
--- src/main/java/com/gitjupalza/domain/chemy/util/ChemyConverter.java
+++ src/main/java/com/gitjupalza/domain/chemy/util/ChemyConverter.java
@@ -2,6 +2,8 @@ package com.gitjupalza.domain.chemy.util;
 
 import com.gitjupalza.domain.chemy.data.dto.ChemyDto;
 import com.gitjupalza.domain.chemy.data.entity.Chemy;
+import com.gitjupalza.domain.chemy.data.event.CreateChemyEvent;
+import com.gitjupalza.domain.chemy.data.event.DeleteChemyEvent;
 import com.gitjupalza.domain.chemy.data.response.QueryChemyResponse;
 
 public interface ChemyConverter {
@@ -9,4 +11,8 @@ public interface ChemyConverter {
     Chemy toEntity(ChemyDto dto);
 
     ChemyDto toDto(Chemy entity);
+
+    CreateChemyEvent toCreateEvent(Chemy entity, Long issuerIdx);
+
+    DeleteChemyEvent toDeleteEvent(Long idx);
 }
